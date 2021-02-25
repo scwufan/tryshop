e9b01740bd3e3f4b7f5593534b07bf02900a73d4
username <2452605610@qq.com>
2021-02-25 17:19:59 +0800

    '初始化仓库’

---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import layout from '../views/layout/layout.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/layout',
+    name: 'layout',
+    component: layout,
+    
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+ <div>
+ 
+ </div>
+</template>
+
+<script>
+import axios from 'axios'
+ export default {
+   name: '',
+   props: {
+   },
+   components: {
+
+   },
+   data () {
+     return {
+
+     }
+   },
+   methods: {
+
+   },
+   mounted() {
+     axios.get('http://192.168.101.98:7001/shoplist/getShop').then((res) =>{
+        console.log(res)
+     })
+   },
+   watch: {
+
+   },
+   computed: {
+
+   }
+ }
+</script>
+
+<style scoped lang='scss'>
+
+</style>
